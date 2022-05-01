56a628ae3d21dcf051b00c6000d61e42a3ac3a33
Ashkan <ashkan.arabim@gmail.com>
2022-04-30 22:57:55 -0600

    saves sample html to sources

--- online-scripts/wiki-vowelless.py
+++ online-scripts/wiki-vowelless.py
@@ -1,9 +1,9 @@
 import requests
-url = "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"
-#just a random link of a dummy file
+url = "https://fa.wikipedia.org/wiki/%D8%AC%D9%86%DA%AF_%D8%BA%D8%B2%D9%87_(%DB%B2%DB%B0%DB%B0%DB%B8%E2%80%93%DB%B2%DB%B0%DB%B0%DB%B9)"
+#link to a test wikipedia article
 r = requests.get(url)
 #retrieving data from the URL using get method
-with open("dummy.pdf", 'wb') as f:
+with open("./sources/dummy.html", 'wb') as f:
 #giving a name and saving it in any required format
 #opening the file in write mode
     f.write(r.content) 
